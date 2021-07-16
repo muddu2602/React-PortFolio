f9acf0dd5e9321002b234ab3add3d92fe2c9b189
muddu2602 <abhishek.muddu26@gmail.com>
2021-07-17 02:38:26 +0530

    handled errors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,9 +30,9 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <h4 href='#' className="text-indigo-400 leading-relaxed">
                 reedbarger@email.com
-              </a>
+              </h4>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
